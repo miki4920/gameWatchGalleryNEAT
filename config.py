faa8478677463acfdf8b6723cf5139f79ec5fd40
miki4920 <miki4920@gmail.com>
2022-09-05 19:19:27 +0100

    Added config file

---
+++ config.py
@@ -0,0 +1,16 @@
+from pygame.locals import *
+
+
+class KeyBinds:
+    UP = K_UP
+    DOWN = K_DOWN
+    RIGHT = K_RIGHT
+    LEFT = K_LEFT
+
+
+class Config:
+    SCREEN_WIDTH = 500
+    SCREEN_HEIGHT = 500
+
+    PLAYER_SIZE = (50, 50)
+    PLAYER_COLOUR = (255, 128, 128)
